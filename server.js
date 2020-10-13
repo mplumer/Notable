cf6c0f392a0318bdf162b9e1cd28d9321c69406e
Max Plumer <68719068+mplumer@users.noreply.github.com>
2020-10-12 21:25:32 -0400

    begin writing post routes

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const { notes } = require('./data/db.json');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 
 
@@ -23,7 +28,13 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+app.post('/api/notes', (req, res) => {
+    // req.body is where our incoming content will be
+  console.log(req.body);
+  res.json(req.body);
+});
 
-app.listen(3003, () => {
-    console.log(`API server now on port 3001!`);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
