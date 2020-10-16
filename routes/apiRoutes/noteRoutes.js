622b313150242358f7f450471eb1269f98128d3d
Max Plumer <68719068+mplumer@users.noreply.github.com>
2020-10-15 20:39:34 -0400

    add id, query functions

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,20 +1,32 @@
 const router = require("express").Router();
-const { createNewNote, validateNote, } = require("../../lib/notes");
-const { notes } = require("../../data/notes");
+const {
+    filterByQuery,
+    findById,
+    createNewNote,
+    validateNote,
+} = require("../../lib/notes");
+const { notes } = require("../../data/db.json");
 
-router.get('/notes', (req, res) => {
-    res.json(notes);
+router.get("/notes", (req, res) => {
+    let results = notes;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
 });
 
-/*app.post('/api/notes', (req, res) => {
-    // req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
-}); */
+router.get("/notes/:id", (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
 
 router.post('/notes', (req, res) => {
     // set id based on what the next index of the array will be
-    //req.body.id = notes.length.toString();*/
+    req.body.id = notes.length.toString();
 
     // if any data in req.body is incorrect, send 400 error back
     if (!validateNote(req.body)) {
